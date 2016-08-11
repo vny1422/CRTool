efe44fa1328845d3e855eb4fa7dfc95d834e3364
Hariyanto <amazingharry95@gmail.com>
2016-08-11 18:59:51 +0700

    vLogin

--- CRTool/application/views/vLogin.php
+++ CRTool/application/views/vLogin.php
@@ -18,8 +18,36 @@
 		</div>
 
 		<div class="row">
-			<div class="col-12">
-				
+			<!--<div class="col-12">
+				<div id="form">
+					<div class="row">
+						<div class="col-7">
+							<img class="center-block" src="<?php echo base_url('images/username.png'); ?>" width="900" alt="" style="max-width:500px; height:auto; max-height:600px;" /> 
+						</div>
+						<div class="col-5">
+							<input type="email">
+						</div>
+					</div>
+					<br>
+					<div class="row">
+						<div class="col-7">
+							<img class="center-block" src="<?php echo base_url('images/password.png'); ?>" width="900" alt="" style="max-width:500px; height:auto; max-height:600px;" />
+						</div>
+						<div class="col-5">
+							<input type="password">
+						</div>
+					</div> 	
+				</div>
+			</div>-->
+			<div class="form-group">
+				<label class="control-label"><img class="center-block" src="<?php echo base_url('images/username.png'); ?>" width="900" alt="" style="max-width:500px; height:auto; max-height:600px;" /> 
+</label>
+				<input class="form-control" name="username" type="text" placeholder="username...">
+			</div>
+			<div class="form-group">
+				<label class="control-label"><img class="center-block" src="<?php echo base_url('images/password.png'); ?>" width="900" alt="" style="max-width:500px; height:auto; max-height:600px;" /> 
+</label>
+				<input class="form-control" name="password" type="password" placeholder="password...">
 			</div>
 		</div>
 		

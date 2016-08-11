690fda8620c21baffcbc8495d5272fd55b380d5b
Hariyanto <amazingharry95@gmail.com>
2016-08-11 13:43:29 +0700

    view login

---
+++ CRTool/application/views/vLogin.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HMTL>
+<html>
+	<head>
+		<script type="text/javascript" src="serverTime.js"></script>
+		<LINK href="grid.css" rel="stylesheet" type="text/css">
+		<style>
+		</style>
+		<title>CR Monitoring</title>
+	</head>
+	<body>
+		<div class="row">
+			<div class="col-12">
+				<div id="date_time"></div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12">
+				<div id="logoPerusahaan">
+					<img class="center-block" src="logoPerusahaan.png">
+					<div id="namaPerusahaan">
+						MONITORING CR INFORMATION SYSTEM
+					</div>
+					<p><hr></p>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-12">
+				
+			</div>
+		</div>
+		
+		
+	</body>
+	<script>
+		window.onload = date_time('date_time');
+	</script>
+</html>

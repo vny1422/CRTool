dcf32b0dc077c71312ace45fa92bb541f8535d26
shoffi <shoffi.izza@gmail.com>
2016-09-26 19:48:33 +0700

    tabel achievement

---
+++ CRTool/application/views/halamanAchievement.php
@@ -0,0 +1,44 @@
+<div class="container">
+  <table class="table table-hover" align="center">
+    <thead>
+      <tr>
+        <th colspan="3">ACHIEVEMENT FOR 6 MONTHS</th>
+      </tr>
+      <tr>
+        <th rowspan="2">MONTH</th>
+        <th colspan="2">SUM OF SN</th>
+        <th colspan="4">SALES OUT</th>
+      </tr>
+      <tr>
+        <th>APPROVE</th>
+        <th>RETUR</th>
+        <th>INCOME</th>
+        <th>TARGET</th>
+        <th>DEFISIT</th>
+        <th>ACHIEVEMENT (%)</th>
+      </tr>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+</div>

f3265c93e6d92b3d26b6e9e49ec502da674b7b5e
shoffi <shoffi.izza@gmail.com>
2016-09-27 11:59:03 +0700

    tabel achievement

---
+++ CRTool/application/controllers/cAchievement.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class cAchievement extends CI_Controller {
+  function __construct()
+    {
+                parent::__construct();
+                $this->load->helper('url_helper');
+               
+     }
+
+  public function index()
+  {
+    $data['title'] = 'Home | CR Monitoring';
+    $data['username'] = 'amazingharry95';
+    $data['fullName'] = '';#ini belum
+    $data['email'] = '';#ini email
+    $this->load->view('templates/vMenu');
+    $this->load->view('templates/headAll', $data);
+    $this->load->view('halamanAchievement');
+    $this->load->view('templates/footer');
+  }
+
+
+}

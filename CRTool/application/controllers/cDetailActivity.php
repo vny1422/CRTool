c0e7380b5a5f168abf1c32d6b62c09aa0963779e
Harry <amazingharry95@gmail.com>
2016-09-29 17:18:41 +0700

    Detail Activity

--- CRTool/application/controllers/cDetailActivity.php
+++ CRTool/application/controllers/cDetailActivity.php
@@ -1,24 +1,24 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class cAchievement extends CI_Controller {
+class cDetailActivity extends CI_Controller {
   function __construct()
-    {
-                parent::__construct();
-                $this->load->helper('url_helper');
+  {
+    parent::__construct();
+    $this->load->helper('url_helper');
                
-     }
+  }
 
   public function index()
   {
-    $data['title'] = 'Achievement | CR Monitoring';
+    $data['title'] = 'Detail Activity | CR Monitoring';
     $data['username'] = 'amazingharry95';
     $data['fullName'] = '';#ini belum
     $data['email'] = '';#ini email
     $data['halamanUtama'] = 0;
     $this->load->view('templates/headAll', $data);
     $this->load->view('templates/vMenu');
-    $this->load->view('halamanAchievement');
+    $this->load->view('halaman');
     //$this->load->view('templates/footer');
   }
 

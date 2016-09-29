e7efc55e850f2cd93ba04c1fe367bbb04079cb6c
Harry <amazingharry95@gmail.com>
2016-09-29 16:31:50 +0700

    background: white, table auto

--- CRTool/application/views/halamanUtama.php
+++ CRTool/application/views/halamanUtama.php
@@ -3,6 +3,6 @@
 
  	</main>
  	
-	 <!--<div id="map_canvas" style="width: 100%; height: 70%; position: relative; overflow: hidden; bottom: 560; background-color: rgb(229, 227, 223);"> <?php echo $map['html']; ?> </div>-->
-	 <div id="map_canvas"></div>
+	 <div id="map_canvas" style="width: 100%; height: 70%; position: relative; overflow: hidden; bottom: 560; background-color: rgb(229, 227, 223);"> <?php echo $map['html']; ?> </div>
+	 <!--<div id="map_canvas"></div>-->
 
